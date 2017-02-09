8bf1fba7bd09baa56c6d6b44e44eb38719db91d7
Szymon <szymon.grotha@gmail.com>
2017-02-09 18:05:54 +0100

    Menu and Shop section fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 	$('#hamburger-menu').click(function() {
-		var jsMenu = $('.none').css('display');
 		$('#mobile-menu-list').toggleClass('mobile-content');
 		$('.none').toggleClass('block');
-
 	});	
 });
