2c85e9d92540b7587387fd2fdf4c006c0dae13a8
Szymon <szymon.grotha@gmail.com>
2017-02-09 19:32:07 +0100

    Animation Work Section

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
 		$('#mobile-menu-list').toggleClass('mobile-content');
 		$('.none').toggleClass('block');
 	});	
+
+	$('.about').waypoint(function() {
+		$('.bike-logo').css('opacity', 1.0).addClass('animated bounceInLeft');
+		$('.vintage-olivia').css('opacity', 1.0).addClass('animated fadeInUp');
+	});
+
+	$('.work').waypoint(function(){
+		$('.la-boriosa').css('opacity', 1.0).addClass('animated fadeInUp');
+		$('.bike-logo2').css('opacity', 1.0).addClass('animated bounceInRight');
+	});
+
+	$('.bike-logo2').waypoint(function(){
+		$('.retro-bike').css('opacity', 1.0).addClass('animated fadeInUp');
+		$('.bike-logo3').css('opacity', 1.0).addClass('animated bounceInLeft');
+	});
 });
+
