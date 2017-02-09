5db490b80c3d79b87b5d04b260c988b72c798544
Szymon <szymon.grotha@gmail.com>
2017-02-09 10:57:35 +0100

    Some Mobile Menu Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,7 @@ $(document).ready(function() {
 	$('#hamburger-menu').click(function() {
 		var jsMenu = $('.none').css('display');
 		$('#mobile-menu-list').toggleClass('mobile-content');
-		
-		if (jsMenu === 'none') {
-			$('.none').css( "display", "block" );
-		} else {
-			$('.none').css( "display", "none" );
-		}	
+		$('.none').toggleClass('block');
+
 	});	
 });
