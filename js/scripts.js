c5e97eae799b9601be4f5f878964ac2dbc43dbdc
Szymon <szymon.grotha@gmail.com>
2017-02-15 12:23:16 +0100

    Menu section + animation fix

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,32 @@ $(document).ready(function() {
 		$('.none').toggleClass('block');
 	});	
 
-	$('.about').waypoint(function() {
+	$('a[href^="#"]').on('click', function() {
+	var target = $(this.getAttribute('href'));
+			event.preventDefault();
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top
+			}, 1000);
+	});
+
+	$('#work').waypoint(function() {
 		$('.bike-logo').css('opacity', 1.0).addClass('animated bounceInLeft');
 		$('.vintage-olivia').css('opacity', 1.0).addClass('animated fadeInUp');
+	}, { 
+		offset: '50%'	
 	});
 
-	$('.work').waypoint(function(){
+	$('.work2').waypoint(function(){
 		$('.la-boriosa').css('opacity', 1.0).addClass('animated fadeInUp');
 		$('.bike-logo2').css('opacity', 1.0).addClass('animated bounceInRight');
+	}, { 
+		offset: '50%'	
 	});
-
-	$('.bike-logo2').waypoint(function(){
+	$('.work3').waypoint(function(){
 		$('.retro-bike').css('opacity', 1.0).addClass('animated fadeInUp');
 		$('.bike-logo3').css('opacity', 1.0).addClass('animated bounceInLeft');
+	}, { 
+		offset: '50%'	
 	});
 });
 
